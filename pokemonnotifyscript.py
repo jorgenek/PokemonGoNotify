5e16aa0909f7609a6e0b7e1bb51f3f9b1cadb59f
Jørgen Ekeland <jorgen.ekeland@gmail.com>
2017-01-18 23:42:44 +0100

    change to the default variable

--- pokemonnotifyscript.py
+++ pokemonnotifyscript.py
@@ -64,16 +64,17 @@ def notifyHitEmail(id, name, lat, lng, attack, defense, stamina):
 # raw_input returns the empty string for "enter"
 yes = set(['yes','y', 'ye', ''])
 no = set(['no','n'])
+defaults = ['lapras', 'dragonite', 'chansey', 'exeggutor', 'snorlax', 'gyarados', 'porygon', 'vaporeon', 'rhydon', 'omastar', 'kabutops']
 
 choice = raw_input("Use default Pokemons? [Yes/No] ").lower()
 if choice in yes:
-   pokemons = ['lapras', 'dragonite', 'chansey', 'exeggutor', 'snorlax', 'gyarados', 'porygon', 'vaporeon', 'rhydon', 'omastar', 'kabutops']
+   pokemons = defaults
 elif choice in no:
    pokemons = formatPokemonsToList(raw_input("Please enter the pokemons you are searching for seperated by ',': "))
 else:
    print("Please respond with 'yes' or 'no'. Using default pokemon")
-   pokemons = ['lapras', 'dragonite', 'chansey', 'exeggutor', 'snorlax', 'gyarados']
-   
+   pokemons = defaults
+
 print pokemons
 fromEmail = addMailEnding(raw_input("Sending Gmail account: "))
 password = getpass.getpass("Password: ")
