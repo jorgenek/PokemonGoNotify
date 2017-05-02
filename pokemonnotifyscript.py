eddd3de5b0b5b0c4517f3eecf64066f5eccb4cc3
Jørgen Ekeland <jorgen.ekeland@gmail.com>
2017-05-02 23:55:42 +0200

    Big clean up after niantic removed pokemon spesific stats and moves

--- pokemonnotifyscript.py
+++ pokemonnotifyscript.py
@@ -58,7 +58,7 @@ def setNoneToZero(value):
 
 def getGender(gender):
     if gender == 1:
-        return "Male";
+        return "Male"
 
     elif gender == 2:
         return "Female"
@@ -66,7 +66,7 @@ def getGender(gender):
     elif gender == 3:
         return "None"
     else:
-        return "Unknown"
+        return None
 
 def formatHeightWeight(value):
     if value is not None:
@@ -103,7 +103,8 @@ def getPokemonTypes(typeList):
 def convertTimestampToTime(timestamp):
     return datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
 
-def notifyDiscovery(id, name, lat, lng, attack, defense, stamina, rarity, types, gender, height, weight, move1, move2, iv, disappear_time):
+def notifyDiscovery(id, name, lat, lng, attack, defense, stamina, rarity, types,
+gender, height, weight, cp, form, move1, move2, iv, disappear_time):
 
     id = formatId(str(id))
     pageurl = 'http://bulbapedia.bulbagarden.net/wiki/File:' + id + str(name) +'.png'
@@ -111,34 +112,16 @@ def notifyDiscovery(id, name, lat, lng, attack, defense, stamina, rarity, types,
     tree = html.fromstring(page.content)
     pokemonImageUrl = tree.xpath('//div[@class="fullImageLink"]//a/@href')[0]
 
-    strongEnoughPokemon = iv >= ivLvl
-    perfect = False
-
-    if iv == 45:
-        perfect = True
-
-    weight = formatHeightWeight(weight) + "kg" if weight is not None else "??"
-    height = formatHeightWeight(height) + "m" if height is not None else "??"
+    weight = formatHeightWeight(weight) + "kg" if weight is not None else None
+    height = formatHeightWeight(height) + "m" if height is not None else None
     genderSign = getGender(gender)
     ivtemp = "{0:.1f}".format((float(iv) / 45) * 100) + " %"
     pokemonDistance = haversine(float(latAnswear), float(lngAnswear), float(lat), float(lng))
-    pokemonNearby = pokemonDistance <= distanceAnswear
     pokemonDistance = "{0:.2f}".format(pokemonDistance) + " km"
     pokemonTypes = getPokemonTypes(types)
 
-    strengthText = "Normal"
-    if perfect:
-        strengthText = "PERFECT"
-    elif iv > 42 and not perfect:
-        strengthText = "Very strong"
-    elif iv <= 42 and iv > 30:
-        strengthText = "Strong"
-    elif iv <= 22:
-        strengthText = "Weak"
-
     print time.strftime("%d. %b %Y %H:%M:%S")
-    print bcolors.OKGREEN + """{name} was discovered with:
-    Strength: {strength}
+    print bcolors.OKGREEN + """#{id} {name} was discovered with:
     IV: {iv}
     Attack: {attack}
     Defense: {defense}
@@ -148,84 +131,112 @@ def notifyDiscovery(id, name, lat, lng, attack, defense, stamina, rarity, types,
     Gender: {gender}
     Height: {height}
     Weight: {weight}
+    Form: {form}
+    CP: {cp}
     Disappears: {disappear}
     Rarity: {rarity}
     Types: {types}
-    Nearby: {nearby}
     Distance: {distance}
     Latitude: {lat}
     Longitude: {lng}
-    """.format(name=name, strength=strengthText, types=pokemonTypes, gender=genderSign, height=height, weight=weight, rarity=rarity, iv=ivtemp, attack=attack, defense=defense, stamina=stamina, nearby=pokemonNearby, move1=move1, move2=move2, distance=pokemonDistance, lat=lat, lng=lng, disappear=disappear_time) + bcolors.ENDC
+    """.format(name=name, types=pokemonTypes, gender=genderSign, height=height,
+    weight=weight, rarity=rarity, iv=ivtemp, attack=attack, defense=defense,
+    stamina=stamina, move1=move1, move2=move2,
+    distance=pokemonDistance, lat=lat, lng=lng, disappear=disappear_time, cp=cp,
+    form=form, id=id) + bcolors.ENDC
 
-    if strongEnoughPokemon or pokemonNearby:
-        if strongEnoughPokemon and pokemonNearby:
-            pokemonDescription = strengthText + " and nearby"
-        elif strongEnoughPokemon and not pokemonNearby:
-            pokemonDescription = strengthText
-        elif not strongEnoughPokemon and pokemonNearby:
-            pokemonDescription = "Nearby"
-        else:
-            pokemonDescription = ""
+    msg = MIMEMultipart('alternative')
+    msg["From"] = fromEmail
+    msg["To"] = toEmail
+    msg["Subject"] = "#" + id + " " + name.upper() + " was discovered!"
 
-        msg = MIMEMultipart('alternative')
-        msg["From"] = fromEmail
-        msg["To"] = toEmail
-        msg["Subject"] = "#" + id + " " + name.upper() + " was found!"
+    body = """
+    ID: {id}
+    Name: {name}
+    Types: {types}
+    Rarity: {rarity}
+    Disappears: {disappear}
+    Distance: {distance}
+    http://maps.google.com/maps?z=8&t=m&q=loc:{lat}+{lng}
+    """.format(name=name, id=id, rarity=rarity, distance=pokemonDistance, lat=lat,
+    lng=lng, types=pokemonTypes, disappear=disappear_time)
 
-        body = """ {description} {name} was discovered with:
-        IV: {iv}
-        Attack: {attack}
-        Defense: {defense}
-        Stamina: {stamina}
-        Strength: {strength}
-        Gender: {gender}
-        Move1: {move1}
-        Move2: {move2}
-        Types: {types}
-        Rarity: {rarity}
-        Disappears: {disappear}
-        Nearby: {nearby}
-        Distance: {distance}
-        http://maps.google.com/maps?z=8&t=m&q=loc:{lat}+{lng}
-        """.format(description=pokemonDescription, name=name, strength=strengthText, gender=genderSign, height=height, weight=weight, types=pokemonTypes, rarity=rarity, iv=ivtemp, attack=attack, defense=defense, stamina=stamina, move1=move1, move2=move2, nearby=pokemonNearby, distance=pokemonDistance, lat=lat, lng=lng, disappear=disappear_time)
+    htmlemail = """
+<html>
+      <body>
+          <div style="margin:0!important;padding:0!important">
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <td bgcolor="#132240" align="center" style="padding-bottom:50px;">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:200px">
+                        <tbody>
+                          <tr>
+                            <td>
+                                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                    <tbody>
+                                    <tr>
+                                        <td>
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                <tbody><tr>
+                                                    <td align="center" style="font-size:25px;font-family:Helvetica,Arial,sans-serif;color:#f2c12e;padding-top:30px">#{id} {name}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="center"><br><img src={url} tabindex="0" width="100"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="center" style="padding:20px 0 0 0;font-size:16px;line-height:25px;font-family:Helvetica,Arial,sans-serif;color:#d8d8d8">Disappears: {disappear}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="center" style="padding:20px 0 0 0;font-size:16px;line-height:25px;font-family:Helvetica,Arial,sans-serif;color:#d8d8d8">Distance: {distance}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="center" style="padding:20px 0 0 0;font-size:16px;line-height:25px;font-family:Helvetica,Arial,sans-serif;color:#d8d8d8">Types: {types}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td align="center" style="padding:20px 0 0 0;font-size:16px;line-height:25px;font-family:Helvetica,Arial,sans-serif;color:#d8d8d8">Rarity: {rarity}</td>
+                                                </tr>
+                                            </tbody></table>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                <tbody><tr>
+                                                    <td align="center" style="padding-top:25px">
+                                                        <table border="0" cellspacing="0" cellpadding="0">
+                                                            <tbody><tr>
+                                                                <td align="center" style="border-radius:3px" bgcolor="#256F9C"><a href="http://maps.google.com/maps?z=8&t=m&q=loc:{lat}+{lng}" style="font-size:16px;font-family:Helvetica,Arial,sans-serif;color:#ffffff;text-decoration:none;color:#ffffff;text-decoration:none;border-radius:3px;padding:15px 25px;border:1px solid #256f9c;display:inline-block" target="_blank">
+                                                                  Get directions</a></td>
+                                                            </tr>
+                                                        </tbody></table>
+                                                    </td>
+                                                </tr>
+                                            </tbody></table>
+                                        </td>
+                                    </tr>
+                                </tbody></table>
+                            </td>
+                        </tr>
+                    </tbody></table>
+                </td>
+            </table>
+          </div>
+      </body>
+  </html>
+    """.format(url=pokemonImageUrl, id=id, name=name, types=pokemonTypes, rarity=rarity,
+    distance=pokemonDistance, lat=lat, lng=lng, disappear=disappear_time)
 
-        htmlemail = """
-        <html>
-            <body>
-                <h2>{description} {name} was discovered</h2>
-                <img src={url} alt="Pokemon image" style="width:35%; float:right; max-width: 200px;">
-                <ul style="list-style-position: inside; list-style-type: none;">
-                <li>IV: {iv}</li>
-                <li>Attack: {attack}</li>
-                <li>Defense: {defense}</li>
-                <li>Stamina: {stamina}</li>
-                <li>Strength: {strength}</li>
-                <li>Gender: {gender}</li>
-                <li>Move1: {move1}</li>
-                <li>Move2: {move2}</li>
-                <li>Types: {types}</li>
-                <li>Rarity: {rarity}</li>
-                <li>Disappears: {disappear}</li>
-                <li>Nearby: {nearby}</li>
-                <li>Distance: {distance}</li>
-                </ul>
-                <h3><a href="http://maps.google.com/maps?z=8&t=m&q=loc:{lat}+{lng}">See on map!</a></h3>
-            </body>
-        </html>
-        """.format(url=pokemonImageUrl, description=pokemonDescription, name=name, strength=strengthText, gender=genderSign, height=height, weight=weight, types=pokemonTypes, rarity=rarity, iv=ivtemp, attack=attack, defense=defense, stamina=stamina, move1=move1, move2=move2, nearby=pokemonNearby, distance=pokemonDistance, lat=lat, lng=lng, disappear=disappear_time)
+    msg.attach(MIMEText(body, "plain"))
 
-        msg.attach(MIMEText(body, "plain"))
+    msg.attach(MIMEText(htmlemail, 'html'))
 
-        msg.attach(MIMEText(htmlemail, 'html'))
-
-        print bcolors.HEADER + "Sending email" + bcolors.ENDC
-        print
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(fromEmail, password)
-        text = msg.as_string()
-        server.sendmail(fromEmail, toEmail, text)
-        server.quit()
+    print bcolors.HEADER + "Sending email" + bcolors.ENDC
+    print
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(fromEmail, password)
+    text = msg.as_string()
+    server.sendmail(fromEmail, toEmail, text)
+    server.quit()
 
     cnt[name] += 1
     seenCount = len(discoveredList)
@@ -245,24 +256,18 @@ print bcolors.OKBLUE + "--------------------------------------------------------
 pokemons = formatPokemonsToList(config["pokemons"])
 latAnswear = str(yourlat) if config["latitude"] == '' else config["latitude"].replace(",", ".")
 lngAnswear = str(yourlng) if config["longitude"] == '' else config["longitude"].replace(",", ".")
-ivLvl = config["ivLvl"]
-distanceAnswear = config["distance"]
 fromEmail = addMailEnding(config['sendingEmail'])
 toEmail = addMailEnding(config['recievingEmail'])
 fromEmail = addMailEnding(config['sendingEmail'])
-notifyPerfect = config['perfect']
 
 print "Pokemons: "
 print bcolors.WARNING + config["pokemons"] + bcolors.ENDC
 print "Latitude: " + bcolors.WARNING + latAnswear + bcolors.ENDC
 print "Longitude: " + bcolors.WARNING + lngAnswear + bcolors.ENDC
-print "IV lvl: " + bcolors.WARNING + str(ivLvl) + bcolors.ENDC
-print "Distance: " + bcolors.WARNING + str(distanceAnswear) + bcolors.ENDC
-print "Notify perfect: " + bcolors.WARNING + str(notifyPerfect) + bcolors.ENDC
 print "Recieving email: " + bcolors.WARNING + toEmail + bcolors.ENDC
 print "Sending email: " + bcolors.WARNING + str(fromEmail) + bcolors.ENDC
 
-password = getpass.getpass("Password for " + addMailEnding(config['sendingEmail']) + ":")
+password = getpass.getpass("Input password for " + addMailEnding(config['sendingEmail']) + ":")
 
 #List of pokemons that are discoverd so you won"t get spammed
 discoveredList = []
@@ -273,17 +278,20 @@ while True:
         pokemonJson = getPokemons()
 
         for i in pokemonJson["pokemons"]:
-            attack = int(setNoneToZero(i["individual_attack"]));
-            defense = int(setNoneToZero(i["individual_defense"]));
-            stamina = int(setNoneToZero(i["individual_stamina"]));
+            attack = int(setNoneToZero(i["individual_attack"]))
+            defense = int(setNoneToZero(i["individual_defense"]))
+            stamina = int(setNoneToZero(i["individual_stamina"]))
             iv = sumIV(attack, defense, stamina)
-            if i["encounter_id"] not in discoveredList :
-                if i["pokemon_name"].lower() in pokemons or iv >= 45 and notifyPerfect :
-                    disappear_time = convertTimestampToTime(int(str(i["disappear_time"])[:-3]))
-                    move1 = getMoveName(str(i["move_1"]))
-                    move2 = getMoveName(str(i["move_2"]))
-                    discoveredList.append(i["encounter_id"])
-                    notifyDiscovery(i["pokemon_id"], i["pokemon_name"], i["latitude"], i["longitude"], i["individual_attack"], i["individual_defense"], i["individual_stamina"], i["pokemon_rarity"], i["pokemon_types"], i["gender"], i["height"], i["weight"], move1, move2, iv, disappear_time)
+            if i["encounter_id"] not in discoveredList and i["pokemon_name"].lower() in pokemons :
+                disappear_time = convertTimestampToTime(int(str(i["disappear_time"])[:-3]))
+                move1 = getMoveName(str(i["move_1"]))
+                move2 = getMoveName(str(i["move_2"]))
+                discoveredList.append(i["encounter_id"])
+                notifyDiscovery(i["pokemon_id"], i["pokemon_name"], i["latitude"],
+                i["longitude"], i["individual_attack"], i["individual_defense"],
+                i["individual_stamina"], i["pokemon_rarity"], i["pokemon_types"],
+                i["gender"], i["height"], i["weight"], i["cp"], i["form"],
+                move1, move2, iv, disappear_time)
     except (ValueError, requests.exceptions.RequestException):
         print bcolors.WARNING + "Error fetching pokemons. Retrying..." + bcolors.ENDC
 
