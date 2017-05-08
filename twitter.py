06da205258f7004a572b1b2b6a01a5d9708218dd
Jørgen Ekeland <jorgen.ekeland@gmail.com>
2017-05-09 00:41:34 +0200

    updated twitter

--- twitter.py
+++ twitter.py
@@ -9,10 +9,12 @@ def twitter_api(cfg):
 
 def tweet(url, id, name, iv, attack, defense, stamina, cp, gender, height,
 weight, types, form, move1, move2, lat, lng, disappear, config):
-    if iv is "0.0 %":
-        tweet = "#" + str(id) + " " + name + " har blitt oppdaget og forsvinner " + disappear + ". IV: " + str(iv) + " % (" + str(attack) + "/" + str(defense) + "/" + str(stamina) + "). CP: " + str(cp) + ". Kjonn: " + gender + ". Moves: " + move1 + ", " + move2 + ". Hoyde: " + str(height) + ". Vekt: " + str(weight) + ". http://maps.google.com/maps?z=8&t=m&q=loc:" + str(lat) + "+" + str(lng)
+    if iv is not "0.0 %":
+        tweet = "#" + str(id) + " " + name + " forsvinner " + disappear + ". IV: " + str(iv) + " (" + str(attack) + "/" + str(defense) + "/" + str(stamina) + "). " + str(cp) + "cp, "+ move1 + ", " + move2 + ", " + gender + ", " + height +", " + weight +". http://maps.google.com/maps?z=8&t=m&q=loc:" + str(lat) + "+" + str(lng)
+        if len(tweet) > 140:
+            tweet = "#" + str(id) + " " + name + " forsvinner " + disappear + ". IV: " + str(iv) + " (" + str(attack) + "/" + str(defense) + "/" + str(stamina) + "). " + str(cp) + "cp, "+ move1 + ", " + move2 + ", " + gender + ". http://maps.google.com/maps?z=8&t=m&q=loc:" + str(lat) + "+" + str(lng)
     else:
-        tweet = "#" + str(id) + " " + name + " har blitt oppdaget og forsvinner " + disappear + ". http://maps.google.com/maps?z=8&t=m&q=loc:" + str(lat) + "+" + str(lng)
+        tweet = "#" + str(id) + " " + name + " forsvinner " + disappear + ". http://maps.google.com/maps?z=8&t=m&q=loc:" + str(lat) + "+" + str(lng)
 
     api = twitter_api(config)
     filename = name + '.jpg'
