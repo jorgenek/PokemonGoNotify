a208fae99e228b498ea1587b3e14e4d84024d584
Jørgen Ekeland <jorgen.ekeland@gmail.com>
2017-06-07 19:27:52 +0200

    fixed issue with color of error message

--- twitter.py
+++ twitter.py
@@ -2,6 +2,16 @@
 
 import tweepy, requests, os
 
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
 def twitter_api(cfg):
     auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
     auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
