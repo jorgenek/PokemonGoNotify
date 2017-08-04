f905e4a020efbeaf393c8c58bfdfbbe10edead86
Jørgen Ekeland <jorgen.ekeland@gmail.com>
2017-08-04 23:04:14 +0200

    added region pokemons

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 config = {
-  'pokemons': """moltres, zapdos, articuno, mew, mewtwo, meganium, typhlosion,
-  feraligatr, togetic, ampharos, unown, heracross, shuckle, delibird, skarmory,
-  houndoom, donphan, smeargle, hitmontop, miltank, blissey, raikou, entei,
-  suicune, lugia, ho-oh, celebi, larvitar, pupitar, tyranitar""",
+  'pokemons': """moltres, zapdos, articuno, mew, mewtwo, meganium,
+  typhlosion, feraligatr, ampharos, unown, heracross, delibird, smeargle,
+  corsola, blissey, raikou, entei, suicune, lugia, ho-oh, celebi, tyranitar,
+  snorlax, dragonite, lapras, chansey, hitmontop, hitmonlee, hitmonchan,
+  togetic, miltank, porygon, aerodactyl, kabutops, omastar,
+  charizard, blastoise, venusaur, kangaskhan, tauros, farfetch'd""",
   'latitude': "",
   'longitude': "",
   'sendingEmail': "", #Must be Gmail account
